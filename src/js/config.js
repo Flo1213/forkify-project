c916906a2eaf7a441c3b29cc92be5dd7a8c0fc1b
Flo1213 <93582542+Flo1213@users.noreply.github.com>
2024-01-27 20:00:01 +0800

    V2

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,6 @@
+// https://forkify-api.herokuapp.com/v2
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = 'afdd1211-6182-4244-81a1-0f4751d5ecbd';
 export const MODAL_CLOSE_SEC = 2.5;
